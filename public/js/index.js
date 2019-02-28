f2e3bf3fd74f1222faf1887026ebbdff6134e655
credd3 <creddy4@sapient.com>
2019-02-28 19:59:49 +0530

    Mutltiple Counters Done using

--- public/js/index.js
+++ public/js/index.js
@@ -1,45 +1,125 @@
 import '../../public/css/index.css';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 import $ from 'jquery';
+import uniqid from 'uniqid';
 
-const counter = (state = 0, action) => {
-    switch(action.type){
+let currentState = [
+    {
+        id: uniqid(),
+        value: 0
+    }
+]
+
+let min = 1,
+    max = 3;
+
+const counter = (state = currentState, action) => {
+    switch (action.type) {
         case "INCREMENT":
-            if(state==10) return state;
-            return state+1;
+            return state.map(counter => {
+                if (counter.id != action.id) return counter;
+                else if (counter.value == "10") return counter;
+                return {
+                    ...counter,
+                    "value": counter.value + 1
+                }
+            })
         case "DECREMENT":
-            if(state==0) return state;
-            return state-1;
+            return state.map(counter => {
+                if (counter.id != action.id) return counter;
+                else if (counter.value == "0") return counter;
+                return {
+                    ...counter,
+                    "value": counter.value - 1
+                }
+            })
         case "RESET":
-            return 0;
+            return state.map(counter => {
+                if (counter.id != action.id) return counter;
+                return {
+                    ...counter,
+                    "value": 0
+                }
+            })
+        case "ADD_COUNTER":
+            let length = state.length;
+            if(length===max) return state;
+            return [
+                ...state,
+                {
+                    id: uniqid(),
+                    value: 0
+                }
+            ]
+        case "DELETE_COUNTER":
+            length = state.length;
+            if(length===1) return state;
+            return state.filter((counter)=>counter.id!=action.id);
         default:
             return state;
     }
 }
 
 let store = createStore(counter);
-$('.value').html(store.getState());
 
-$('.increment').click(() => {
+$('.counters').on('click','.increment', (e) => {
     store.dispatch({
-        "type":"INCREMENT"
+        "type": "INCREMENT",
+        "id": e.target.parentElement.parentElement.id
     })
 })
 
-$('.decrement').click(() => {
+$('.counters').on('click','.decrement', (e) => {
     store.dispatch({
-        "type":"DECREMENT"
+        "type": "DECREMENT",
+        "id": e.target.parentElement.parentElement.id
     })
 })
 
-$('.reset').click(() => {
+$('.counters').on('click','.reset', (e) => {
     store.dispatch({
-        "type":"RESET"
+        "type": "RESET",
+        "id" : e.target.parentElement.parentElement.id
+    })
+})
+
+$('.counters').on('click','.delete', (e) => {
+    store.dispatch({
+        "type": "DELETE_COUNTER",
+        "id" : e.target.parentElement.parentElement.id
+    })
+})
+
+$('.add').on('click',() => {
+    store.dispatch({
+        "type": "ADD_COUNTER",
     })
 })
 
 store.subscribe(() => {
-    $('.value').html(store.getState())
+    let html=``;
+    store.getState().forEach(counter => {
+        html += `<div class="counter" id="${counter.id}">
+        <span class="value">${counter.value}</span>
+        <div class="buttonContainer">
+            <button class="increment">&plus;</button>
+            <button class="decrement">&minus;</button>
+            <button class="reset">RESET</button>
+            <button class="delete">&#9986;</button>
+        </div>
+    </div>`;
+    });
+    $('.counters').html(html);
 })
+
+$('.counters').append(`<div class="counter" id="${currentState[0].id}">
+<span class="value">${currentState[0].value}</span>
+<div class="buttonContainer">
+    <button class="increment">&plus;</button>
+    <button class="decrement">&minus;</button>
+    <button class="reset">RESET</button>
+    <button class="delete">&#9986;</button>
+</div>
+</div>`);
 
 // In MutlipleCounter Branch
